ca3e9b60e10d57277ef472b582611633f8dfc2e2
Abdirahim Farah <c0009337@hallam.shu.ac.uk>
2026-03-23 06:38:38 +0000

    Added Transaction Model

---
+++ Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BudgetTracker.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, 1000000)]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
